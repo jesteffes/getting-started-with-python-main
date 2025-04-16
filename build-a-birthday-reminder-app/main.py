545b0c0a842cd58b612ff18ff0c9dd39ab267b50
Jenna Steffes <168142203+jesteffes@users.noreply.github.com>
2025-04-16 15:50:47 -0700

    If, elif, else, statements

--- build-a-birthday-reminder-app/main.py
+++ build-a-birthday-reminder-app/main.py
@@ -1,1 +1,10 @@
 user_input = input("Which birthday are you looking for? ")
+
+if user_input == "Mali":
+    print("Birthday is 08/09/08!")
+elif user_input == "Raider":
+    print("Birthday is 11/22/07!")
+elif user_input == "Jack":
+    print("Birthday is 09/17/05!")
+else:
+    print("Can't find a birthday for this person.")
