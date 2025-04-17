f3cd50e7083288a1c2b9136314fd288696f24f09
Jenna Steffes <168142203+jesteffes@users.noreply.github.com>
2025-04-17 15:40:52 -0700

    Updating friends list

---
+++ updating-this-list/main.py
@@ -0,0 +1,11 @@
+names = ["Mali", "Raider", "Jack", "Buddy", "Chandler", "Monica", "Ross", "Rachel", "Joey", "Phoebe", "Emma", "Erica", "Ted", "Robin", "Barney", "Lily", "Marshall", "Marvin", "Penny", "Luke"]
+
+group1 = names[:4]
+group2 = names[16:]
+group3 = names[10:14]
+group4 = names[1]
+
+print(group1)
+print(group2)
+print(group3)
+print(group4)
