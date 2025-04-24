0899b62f195ac450ce4a517578e117e3bf6c6117
Jenna Steffes <168142203+jesteffes@users.noreply.github.com>
2025-04-24 13:44:49 -0700

    Creative with the turtle module

--- get-creative-with-the-turtle-module/main.py
+++ get-creative-with-the-turtle-module/main.py
@@ -27,12 +27,24 @@ turtle.pendown()
 
 def star():
   # Star
+  for i in range(0,5):
+    turtle.pencolor("yellow")
+    turtle.forward(110)
+    turtle.right(216)
 
 def square():
   # Square
+  for i in range(0,4):
+    turtle.pencolor("blue")
+    turtle.forward(100)
+    turtle.right(90)
 
 def hexagon():
   # Hexagon
+  for i in range(0,6):
+    turtle.pencolor("brown")
+    turtle.forward(100)
+    turtle.right (60)
 
 selection = input("1. Star\n2. Square\n3. Hexagon\nSelect a number: ")
 if selection == "1":
