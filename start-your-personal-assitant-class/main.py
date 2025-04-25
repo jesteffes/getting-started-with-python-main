e31448f976a30054383b0ad2002d6ce5d0c24530
Jenna Steffes <168142203+jesteffes@users.noreply.github.com>
2025-04-25 11:16:07 -0700

    Class design for personal assistant app

--- start-your-personal-assitant-class/main.py
+++ start-your-personal-assitant-class/main.py
@@ -1,12 +1,22 @@
 # Give the class a name
-class:
+class PersonalAssistant:
   # Add an __init__ function here
+  def __init__(self):
+    self.contacts = {
+      'Ann': 'Marketing Coordinator', 'Chelsea': 'Software Developer','Nichole': 'Sales Representative', 'Max': 'Technical Writer'
+    }
+    self.todos = []
 
   # Complete the get_contact function code
   def get_contact(self, name):
+    if name in self.contacts: 
+      return self.contacts[name]
+    else:
+      return "There is no contact with that name."
+       
 
 
 # Code to test output of the class
 assistant = PersonalAssistant()
 # Change name to one from your contacts
-print(assistant.get_contact("Chelsea"))
+print(assistant.get_contact("Max"))
